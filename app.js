f40db917fcc9e22b65665e5adf52295d6246e091
Nathan Kwok <sciencenatekwok@gmail.com>
2024-02-18 05:18:43 -0800

    working version v3

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(bodyParser.json());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
