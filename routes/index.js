333da3f937c2b6e59bf838f1b659fa5bd11018a4
Nathan Kwok <sciencenatekwok@gmail.com>
2024-02-17 17:04:11 -0800

    most things are still happening

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {blacklist: [{'asdf':'j'}]});
 });
 router.get('/products', function(req, res, next) {
-  res.render('products', { title: 'Express' });
+  res.render('products');
 });
-router.get('/loading', function(req, res, next) {
-  res.render('loading', { title: 'Express' });
+router.get('/:id', async function(req, res, next) {
+  const id = req.params['id'];
+  // await fetch(process.env.BACKEEND_URL, {
+  //   method: "POST",
+  //   body: JSON.stringify(body),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   }
+  // })
+  // .then(response => response.json()).then(data => console.log(data));
+  res.render('loading');
 });
 console.log('restart done');
 
